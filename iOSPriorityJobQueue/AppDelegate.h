0df6bd282506184bf5c5dde69bce18b23e5d781d
smalllixin <smalllixin@gmail.com>
2014-10-23 09:49:25 +0800

    Initial Commit

---
+++ iOSPriorityJobQueue/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  iOSPriorityJobQueue
+//
+//  Created by lixin on 10/23/14.
+//  Copyright (c) 2014 lxtap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ iOSPriorityJobQueue/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSPriorityJobQueue
+//
+//  Created by lixin on 10/23/14.
+//  Copyright (c) 2014 lxtap. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
